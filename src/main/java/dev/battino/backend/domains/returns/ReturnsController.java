6c8c265f237f8b447a4370a8139db9b206e1d730
Battino <battino46@gmail.com>
2024-03-08 13:07:56 +0100

    init project

---
+++ src/main/java/dev/battino/backend/domains/returns/ReturnsController.java
@@ -0,0 +1,8 @@
+package dev.battino.backend.domains.returns;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ReturnsController {
+    
+}
---
+++ src/test/java/dev/battino/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.battino.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
