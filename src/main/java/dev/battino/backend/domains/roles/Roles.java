6b4b67fa31080b2d1c93e8d5e42706b18e8fcc83
Battino <battino46@gmail.com>
2024-03-12 19:47:43 +0100

    add Roles entity

--- src/main/java/dev/battino/backend/domains/roles/Roles.java
+++ src/main/java/dev/battino/backend/domains/roles/Roles.java
@@ -1,8 +1,38 @@
 package dev.battino.backend.domains.roles;
 
+
+import dev.battino.backend.domains.authority.Authorities;
+import dev.battino.backend.domains.user.User;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lombok.Getter;
+import lombok.Setter;
+import jakarta.persistence.JoinColumn;
+import java.util.Set;
 
 @Entity
+@Getter
+@Setter
 public class Roles {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "roles_id")
+    private Long id;
+
+    @JoinTable(name = "roles_authorities"
+            , joinColumns = @JoinColumn(name = "roles_id")
+            , inverseJoinColumns = @JoinColumn(name = "authorities_id"))
+    @ManyToMany
+    private Set<Authorities> authorities;
+
+    @OneToMany(mappedBy = "role")
+    private Set<User> users;
 }
