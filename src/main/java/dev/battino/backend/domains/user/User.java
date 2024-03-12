098fcdb71ab13a3a12972a1321e3d545c20e9f3d
Battino <battino46@gmail.com>
2024-03-12 19:47:52 +0100

    Refactor User class: Add name field and change role type to Roles

--- src/main/java/dev/battino/backend/domains/user/User.java
+++ src/main/java/dev/battino/backend/domains/user/User.java
@@ -1,16 +1,19 @@
 package dev.battino.backend.domains.user;
 
-import javax.management.relation.Role;
 
+import org.hibernate.mapping.Set;
 import org.hibernate.validator.constraints.Length;
 
+import dev.battino.backend.domains.roles.Roles;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Getter
@@ -21,11 +24,12 @@ public class User {
     private Integer id;
     @NotNull
     @Length(min = 3, max = 20)
-    private String username;
+    private String name;
+    
     @NotNull
     private String password;
 
     @ManyToOne
     @NotNull
-    private Role role;
+    private Roles role;
 }
