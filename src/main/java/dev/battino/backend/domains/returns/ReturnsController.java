3a61d7d64f55cd59443ad2547dbad91b1e8d0416
Battino <battino46@gmail.com>
2024-03-12 21:34:54 +0100

    Add pre-authorization for GET method

--- src/main/java/dev/battino/backend/domains/returns/ReturnsController.java
+++ src/main/java/dev/battino/backend/domains/returns/ReturnsController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -15,6 +16,8 @@ public class ReturnsController {
 
     @Autowired
     private ReturnsRepository returnsRepository;
+
+    @PreAuthorize("hasAuthority('READ')")
     @GetMapping("/{id}")
     public ResponseEntity<String> getMethodName(@PathVariable Integer id) {
         try {
