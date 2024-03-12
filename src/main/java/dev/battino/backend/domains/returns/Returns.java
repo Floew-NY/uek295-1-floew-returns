4badad60332d7d589dadfaf958751267c4f034b3
Battino <battino46@gmail.com>
2024-03-12 21:34:20 +0100

    add required fields for Returns Entity

--- src/main/java/dev/battino/backend/domains/returns/Returns.java
+++ src/main/java/dev/battino/backend/domains/returns/Returns.java
@@ -1,16 +1,34 @@
 package dev.battino.backend.domains.returns;
 
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 @Entity
+@Table(name = "returns")
 public class Returns {
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "returns_id")
     private Integer id;
 
+    @NotNull
+    @Size(min = 3, max = 255)
+    @Column(name = "returns_reason")
     private String reason;
+
+    @NotNull
+    @Column(name = "returns_amount")
+    private long amount;
+
+    @NotNull
+    @Column(name = "returns_order_id")
+    private int orderId;
 }
