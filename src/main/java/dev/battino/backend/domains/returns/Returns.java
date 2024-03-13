a0018d65b32b521f4c8cc244672b5d9cdc3c9eda
Battino <battino46@gmail.com>
2024-03-13 18:58:12 +0100

    fix Sequential startup erros

--- src/main/java/dev/battino/backend/domains/returns/Returns.java
+++ src/main/java/dev/battino/backend/domains/returns/Returns.java
@@ -19,7 +19,7 @@ import lombok.Setter;
 public class Returns {
     
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "returns_id")
     private Integer id;
 
